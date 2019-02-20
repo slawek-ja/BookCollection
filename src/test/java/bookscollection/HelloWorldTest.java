2633a38c6847dc6b8e958f94d6c06d296538772b
slawek-ja <p.slawek90@gmail.com>
2019-02-20 03:16:20 +0100

    #3 - Added Jacoco dependency

--- src/test/java/bookscollection/HelloWorldTest.java
+++ src/test/java/bookscollection/HelloWorldTest.java
@@ -1,10 +1,23 @@
 package bookscollection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
 class HelloWorldTest {
+
+  @Test
+  void shouldInvokeClass() {
+    //given
+    HelloWorld helloWorld;
+
+    //when
+    helloWorld = new HelloWorld();
+
+    //then
+    assertNotNull(helloWorld);
+  }
 
   @Test
   void shouldReturnValidString() {
