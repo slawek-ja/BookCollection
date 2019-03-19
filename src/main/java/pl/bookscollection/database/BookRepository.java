eb2c6fb3dacbc19cbe0b48a8188be148ebe03b21
slawek-ja <p.slawek90@gmail.com>
2019-03-19 18:17:55 +0100

    #15 - Changes after review

--- src/main/java/pl/bookscollection/database/BookRepository.java
+++ src/main/java/pl/bookscollection/database/BookRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import pl.bookscollection.model.Book;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
+
   @Modifying
   @Query(value = "Delete from book where id = ?1", nativeQuery = true)
   void deleteById(Long id);
@@ -23,4 +24,5 @@ public interface BookRepository extends CrudRepository<Book, Long> {
   @Modifying
   @Query(value = "Delete from author", nativeQuery = true)
   void deleteAuthor();
+
 }
