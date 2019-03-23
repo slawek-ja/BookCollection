1a234843bdb71ece57edbb64139e39185cd2a59f
slawek-ja <p.slawek90@gmail.com>
2019-03-23 22:43:10 +0100

    #17 - Added hibernate-validators dependency in pom.xml, Created validators to Author class and created proper tests

--- src/test/java/pl/bookscollection/unittests/model/AuthorTest.java
+++ src/test/java/pl/bookscollection/unittests/model/AuthorTest.java
@@ -4,10 +4,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Set;
+import java.util.stream.Stream;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import pl.bookscollection.model.Author;
 
 class AuthorTest {
+
+  private ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+  private Validator validator;
+
+  @BeforeEach
+  void setUpValidator() {
+    validator = factory.getValidator();
+  }
 
   @Test
   void shouldInvokeClass() {
@@ -46,5 +64,103 @@ class AuthorTest {
   @Test
   void shouldThrowNullPointerExceptionWhenLastNameIsNull() {
     assertThrows(NullPointerException.class, () -> new Author("Tim", null));
+  }
+
+  @ParameterizedTest
+  @MethodSource("getArgumentsForShouldCatchInvalidArgumentsWhenNameAndLastNameAreEmptyTest")
+  void shouldCatchInvalidArgumentsWhenNameAndLastNameAreEmpty(String name, String lastName) {
+    //given
+    Author authorToValidate = new Author(name, lastName);
+    int expectedViolationsSize = 4;
+
+    //when
+    Set<ConstraintViolation<Author>> expectedViolations = validator.validate(authorToValidate);
+
+    //then
+    assertEquals(expectedViolationsSize, expectedViolations.size());
+  }
+
+  private static Stream<Arguments> getArgumentsForShouldCatchInvalidArgumentsWhenNameAndLastNameAreEmptyTest() {
+    return Stream.of(
+            Arguments.arguments("", ""),
+            Arguments.arguments("   ", "   "),
+            Arguments.arguments("  ", ""),
+            Arguments.arguments("", "  "));
+  }
+
+  @ParameterizedTest
+  @MethodSource("getArgumentsForShouldCatchInvalidArgumentsWhenNameIsWrong")
+  void shouldCatchInvalidArgumentsWhenNameIsWrong(String name) {
+    //given
+    Author authorToValidate = new Author(name, "Fox");
+    int expectedViolationsSize = 1;
+    String expectedValidationMessage = "Invalid name, check passed data";
+
+    //when
+    Set<ConstraintViolation<Author>> expectedViolations = validator.validate(authorToValidate);
+    String resultValidationMessage = expectedViolations.iterator().next().getMessage();
+    int resultViolationsSize = expectedViolations.size();
+
+    //then
+    assertEquals(expectedViolationsSize, resultViolationsSize);
+    assertEquals(expectedValidationMessage, resultValidationMessage);
+  }
+
+  private static Stream<Arguments> getArgumentsForShouldCatchInvalidArgumentsWhenNameIsWrong() {
+    return Stream.of(
+            Arguments.arguments("!@#$%^&*()<>?/';"),
+            Arguments.arguments("Abb     "),
+            Arguments.arguments("    Steve"),
+            Arguments.arguments("Steve!@#"),
+            Arguments.arguments("Steven12345"));
+  }
+
+  @ParameterizedTest
+  @MethodSource("getArgumentsForShouldCatchInvalidArgumentsWhenLastNameIsWrong")
+  void shouldCatchInvalidArgumentsWhenLastNameIsWrong(String lastName) {
+    //given
+    Author authorToValidate = new Author("Steve", lastName);
+    int expectedViolationsSize = 1;
+    String expectedValidationMessage = "Invalid last name, check passed data";
+
+    //when
+    Set<ConstraintViolation<Author>> expectedViolations = validator.validate(authorToValidate);
+    String resultValidationMessage = expectedViolations.iterator().next().getMessage();
+    int resultViolationsSize = expectedViolations.size();
+
+    //then
+    assertEquals(expectedViolationsSize, resultViolationsSize);
+    assertEquals(expectedValidationMessage, resultValidationMessage);
+  }
+
+  private static Stream<Arguments> getArgumentsForShouldCatchInvalidArgumentsWhenLastNameIsWrong() {
+    return Stream.of(
+            Arguments.arguments("!@#$%^&*()<>?/';"),
+            Arguments.arguments("Abb     "),
+            Arguments.arguments("    Fox"),
+            Arguments.arguments("Foxy!@#"),
+            Arguments.arguments("Foxy12345"));
+  }
+
+  @ParameterizedTest
+  @MethodSource("getArgumentsForShouldPassValidationProcess")
+  void shouldPassValidationProcess(String name, String lastName) {
+    //given
+    Author authorToValidate = new Author(name, lastName);
+    int expectedViolationsSize = 0;
+
+    //when
+    Set<ConstraintViolation<Author>> expectedViolations = validator.validate(authorToValidate);
+    int resultViolationsSize = expectedViolations.size();
+
+    //then
+    assertEquals(expectedViolationsSize, resultViolationsSize);
+  }
+
+  private static Stream<Arguments> getArgumentsForShouldPassValidationProcess() {
+    return Stream.of(
+            Arguments.arguments("Steve", "Fox"),
+            Arguments.arguments("Stan", "Winfield I"),
+            Arguments.arguments("Dan Steven", "Ground"));
   }
 }
