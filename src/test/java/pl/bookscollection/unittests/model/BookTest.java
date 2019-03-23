7d50e5059b23a00642b05b78a3b1eecff48a2a52
slawek-ja <p.slawek90@gmail.com>
2019-03-24 00:57:08 +0100

    #17 - Created validators for Book class and created proper tests

--- src/test/java/pl/bookscollection/unittests/model/BookTest.java
+++ src/test/java/pl/bookscollection/unittests/model/BookTest.java
@@ -4,12 +4,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import pl.bookscollection.generators.BookGenerator;
 import pl.bookscollection.model.Author;
 import pl.bookscollection.model.Book;
 import pl.bookscollection.model.Cover;
+import java.util.Set;
+import java.util.stream.Stream;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 class BookTest {
+
+  private ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+  private Validator validator;
+
+  @BeforeEach
+  void setUpValidator() {
+    validator = factory.getValidator();
+  }
 
   @Test
   void shouldInvokeClass() {
@@ -58,5 +77,50 @@ class BookTest {
   @Test
   void shouldThrowNullPointerExceptionWhenAuthorIsNull() {
     assertThrows(NullPointerException.class, () -> new Book("Green Woods", Cover.SOFT, null));
+  }
+
+  @ParameterizedTest
+  @MethodSource("getArgumentsForShouldCatchInvalidArgumentsWhenTitleIsEmpty")
+  void shouldCatchInvalidArgumentsWhenTitleIsEmpty(String title) {
+    //given
+    Book bookToValidate = BookGenerator.getBookWithSpecifiedTitle(title);
+    int expectedViolationsSize = 1;
+    String expectedValidationMessage = "Title of the book cannot be empty";
+
+    //when
+    Set<ConstraintViolation<Book>> expectedViolations = validator.validate(bookToValidate);
+    String resultValidationMessage = expectedViolations.iterator().next().getMessage();
+    int resultViolationsSize = expectedViolations.size();
+
+    //then
+    assertEquals(expectedViolationsSize, resultViolationsSize);
+    assertEquals(expectedValidationMessage, resultValidationMessage);
+  }
+
+  private static Stream<Arguments> getArgumentsForShouldCatchInvalidArgumentsWhenTitleIsEmpty() {
+    return Stream.of(
+            Arguments.arguments(""),
+            Arguments.arguments("   "));
+  }
+
+  @ParameterizedTest
+  @MethodSource("getArgumentsForShouldPassValidationProcess")
+  void shouldPassValidationProcess(String title) {
+    //given
+    Book bookToValidate = BookGenerator.getBookWithSpecifiedTitle(title);
+    int expectedViolationsSize = 0;
+
+    //when
+    Set<ConstraintViolation<Book>> expectedViolations = validator.validate(bookToValidate);
+    int resultViolationsSize = expectedViolations.size();
+
+    //then
+    assertEquals(expectedViolationsSize, resultViolationsSize);
+  }
+
+  private static Stream<Arguments> getArgumentsForShouldPassValidationProcess() {
+    return Stream.of(
+            Arguments.arguments("Some title"),
+            Arguments.arguments("SampleTitle"));
   }
 }
