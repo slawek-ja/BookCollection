c2700beb15f1eb34c33cb5eb34585625d431b2e4
slawek-ja <p.slawek90@gmail.com>
2019-03-24 22:29:40 +0100

    #17 - Fixes after review

--- src/main/java/pl/bookscollection/model/Book.java
+++ src/main/java/pl/bookscollection/model/Book.java
@@ -12,7 +12,6 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
